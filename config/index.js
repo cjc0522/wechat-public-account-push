494cfca80fed92314e2516b9d6c516942bfb0f59
cjc0522 <87306532+cjc0522@users.noreply.github.com>
2022-09-02 09:32:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -19,10 +19,10 @@ export const config = {
     /**
      * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
      * [
-     *   {name: '老婆0', id: "okgsU5zg4k5UMPQIX1X3MSXYd_vE"}, 
+     *   {name: '老婆0', id: "okgsU5zg4k5UMPQIX1X3MSXYd_vE,okgsU56aqjrahihQJNkktLRgNdvE"}, 
      */
     USERS: [
-      {name: '老婆0', id: "okgsU5zg4k5UMPQIX1X3MSXYd_vE"}, 
+      {name: '老婆0', id: "okgsU5zg4k5UMPQIX1X3MSXYd_vE,okgsU56aqjrahihQJNkktLRgNdvE"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
